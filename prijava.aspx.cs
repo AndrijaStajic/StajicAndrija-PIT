1b9ace06a4258f68d68e73c9a18db848016cda7e
AndrijaStajic <153907409+AndrijaStajic@users.noreply.github.com>
2023-12-15 12:49:07 +0100

    Add files via upload

---
+++ prijava.aspx.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace AndrijaStajic_III_2
+{
+    public partial class prijava : System.Web.UI.Page
+    {
+        public string Ime
+        {
+            get { return txtIme.Text; } 
+        }
+        public string Email
+        {
+            get { return txtEmail.Text; }
+        }
+        public string Godina
+        {
+            get { return txtGodina.Text; }
+        }
+        public string Razred
+        {
+            get { return rblRazred.Text; }
+        }
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        protected void btnPRIJAVA_Click(object sender, EventArgs e)
+        {
+            if(txtImeValidator.IsValid && txtEmailValidator.IsValid &&
+                txtEmailExpression.IsValid && txtPotvrdaValidator.IsValid &&
+                PorediEmailValidator.IsValid && txtGodinaValidator.IsValid &&
+                GodineValidator.IsValid && rblRazredValidator.IsValid)
+            {
+                btnPRIJAVA.PostBackUrl = "~/izvestaj.aspx";
+                lblPoruka.Text = "Ispravno su popunjena sva polja.";
+            } 
+            else
+            {
+                lblPoruka.Text = "Neka polja nisu ipravno popunjena!";
+            }
+            
+        }
+    }
+}
